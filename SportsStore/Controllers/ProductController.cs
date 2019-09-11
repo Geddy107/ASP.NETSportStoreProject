b976fb8f922a000f479a2e4fd3e4b989ff3cfe1d
John Geddeis <JohnGeddeis@outlook.com>
2019-09-11 08:32:35 -0400

    Beginning adding pagination

--- SportsStore/Controllers/ProductController.cs
+++ SportsStore/Controllers/ProductController.cs
@@ -9,12 +9,20 @@ namespace SportsStore.Controllers
 {
     public class ProductController : Controller
     {
+        
         private IProductRepository repository;
+        public int PageSize = 4;
 
         public ProductController(IProductRepository repo)
         {
             repository = repo;
         }
+
+        public ViewResult List(int productPage = 1) =>
+               View(repository.Products
+                              .OrderBy(p => p.ProductID)
+                              .Skip((productPage - 1) * PageSize)
+                              .Take(PageSize));
 
         public ViewResult List() => View(repository.Products);
     }
