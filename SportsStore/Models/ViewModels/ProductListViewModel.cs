9ddf7e2367dffa21fbe77b001111c16521e92853
John Geddeis <JohnGeddeis@outlook.com>
2019-09-11 10:23:03 -0400

    ProductListViewModel created to give page information

--- SportsStore/Models/ViewModels/ProductListViewModel.cs
+++ SportsStore/Models/ViewModels/ProductListViewModel.cs
@@ -7,5 +7,7 @@ namespace SportsStore.Models.ViewModels
 {
     public class ProductListViewModel
     {
+        public IEnumerable<Product> Products { get; set; }
+        public PagingInfo PagingInfo { get; set; }
     }
 }
